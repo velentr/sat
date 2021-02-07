c6fd7ccb31513519c7b9534f89fd64a421c2cb25
Brian Kubisiak <brian@kubisiak.com>
2021-02-06 21:21:58 -0800

    pset: inline pset functions
    Include pset functions directly in the sat.c file. This allows better
    cross-function optimizations and inlining for roughly 15% speed
    increase.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- debug.sh
+++ debug.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-gcc sat.c pset.c -O0 -g -std=c99 -pedantic -Wall -Wextra -Werror -o sat.debug
-gcc pset_test.c pset.c -O0 -g -std=c99 -pedantic -Wall -Wextra -Werror -o pset.test
+gcc sat.c -O0 -g -std=c99 -pedantic -Wall -Wextra -Werror -o sat.debug
